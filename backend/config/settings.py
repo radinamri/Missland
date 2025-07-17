6d43c8470d8d8118ffe3c0681086b79dd6d4466d
Radin Amri <radin.amrihesari@gmail.com>
2025-07-17 11:03:29 +0330

    fix(config): Configure CORS middleware and settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     # 3rd party apps
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
